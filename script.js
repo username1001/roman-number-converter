dfa1b3aaa0862dfe05b685d1ff09cd7a60b043d9
Matt Fay <username1001@users.noreply.github.com>
2017-08-06 11:28:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-
 function convertToRoman(num) {
-
   num = document.getElementById("enteredNum").value;
   if(num === '' || isNaN(num)) {
     alert("Please enter a number.");
     return false;
   }
-  // Array containing Roman numerals.
-  var roman_numbers = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-
-  // Array containing modern numbers.  
+  // Arrays that contain the two number systems for conversion.
+  var roman_numbers = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']; 
   var modern_numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-  // Converted is where the number will appear.
+  // Converted is where the Roman numeral will appear.
   var converted = '';  
 
   // Run through the modern numbers array
   for(var i = 0; i < modern_numbers.length; i++) {
 
-    // check if the current modern number is less than or equal to num
+    // check if the current modern number is less than or equal to num(the entered number).
      while (modern_numbers[i] <= num) {
 
        // and add it into converted string as a roman number
@@ -27,8 +23,10 @@ function convertToRoman(num) {
     }
   }
   
+ // Displaying the Roman numeral.
  document.getElementById("display").innerHTML = '<p id="romano">' + converted.fontsize(5) + '</p>';
-
+  
+ // Some styling. Gold and red, Roman colors. 
  document.getElementById("romano").style.border = "1px solid black";
  document.getElementById("romano").style.color = "#FBE673";
  document.getElementById("romano").style.backgroundColor = "#9E3818";
