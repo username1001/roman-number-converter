0e5b497215157f9059104a8e53998c8dff825fff
Matt Fay <username1001@users.noreply.github.com>
2017-08-17 17:20:59 -0400

    update to ES6

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function convertToRoman(num) {
   }
   
   // Arrays that contain the two number systems for conversion.
-  var roman_numbers = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']; 
-  var modern_numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman_numbers = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']; 
+  const modern_numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
   // Converted is where the Roman numeral will appear.
-  var converted = '';  
+  let converted = '';  
 
   // Run through the modern numbers array
-  for (var i = 0; i < modern_numbers.length; i++) {
+  for (let i = 0; i < modern_numbers.length; i++) {
 
     // check if the current modern number is less than or equal to num(the entered number).
      while (modern_numbers[i] <= num) {
